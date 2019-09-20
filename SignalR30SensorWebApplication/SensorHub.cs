f2987d4b9d0fb976ac5488809f0eed05dc4337c6
Stephen Halter <halter73@gmail.com>
2019-09-19 17:52:09 -0700

    Add SensorHub

---
+++ SignalR30SensorWebApplication/SensorHub.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR30SensorWebApplication
+{
+    public class SensorHub : Hub
+    {
+        public async IAsyncEnumerable<double> GetSensorData(string sensorName, CancellationToken cancellationToken)
+        {
+            var rng = new Random();
+            double currentTemp = -18;
+            double tempVelocity = 0.5;
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                tempVelocity += (rng.NextDouble() - .5) / 4;
+                tempVelocity = Math.Clamp(tempVelocity, -2, 2);
+
+                currentTemp += tempVelocity;
+
+                if (currentTemp < -30)
+                {
+                    tempVelocity = .5; 
+                }
+                else if(currentTemp > 0)
+                {
+                    tempVelocity = -.5;
+                }
+
+                yield return currentTemp;
+
+                await Task.Delay(1000, cancellationToken);
+            }
+        }
+    }
+}
--- SignalR30SensorWebApplication/Startup.cs
+++ SignalR30SensorWebApplication/Startup.cs
@@ -16,6 +16,7 @@ namespace SignalR30SensorWebApplication
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +27,14 @@ namespace SignalR30SensorWebApplication
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapHub<SensorHub>("/sensor");
             });
         }
     }
